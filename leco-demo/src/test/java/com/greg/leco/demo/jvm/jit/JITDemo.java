46434de531939fac559ebeb6dab2449aca8bc4b1
greg <lecoboy@163.com>
2021-10-18 02:07:33 +0800

    commit

---
+++ leco-demo/src/test/java/com/greg/leco/demo/jvm/jit/JITDemo.java
@@ -0,0 +1,22 @@
+package com.greg.leco.demo.jvm.jit;
+
+/**
+ * 逃逸分析
+ * -XX:-DoEscapeAnalysis
+ *
+ * @author greg
+ * @version 2021/10/17 16:45
+ */
+public class JITDemo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 200; i++) {
+            long start = System.nanoTime();
+            for (int j = 0; j < 1000; j++) {
+                new Object();
+            }
+            long end = System.nanoTime();
+            System.out.printf("%d\t%d\n", i, (end - start));
+        }
+    }
+}
---
+++ leco-demo/src/test/java/com/greg/leco/demo/jvm/jmm/JMMDemo1.java
@@ -0,0 +1,34 @@
+package com.greg.leco.demo.jvm.jmm;
+
+/**
+ * 原子性
+ * i++,i-- 不是原子性操作
+ * @author greg
+ * @version 2021/10/18 0:06
+ */
+public class JMMDemo1 {
+
+    volatile static int i = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+            for (int j = 0; j < 5000; j++) {
+                i++;
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int j = 0; j < 5000; j++) {
+                i--;
+            }
+        });
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        // volatile是保证了可见性，但不保证原子性，所以这里输出结果不一定是0
+        System.out.println(i);
+    }
+}
